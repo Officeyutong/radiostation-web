a1473f69da99ad52e118b961fc6a70a65094f014
officeyutong <officeyutong@163.com>
2020-09-10 22:55:14 +0800

    底部信息

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,6 +3,7 @@ import Router from "./Router";
 import 'semantic-ui-css/semantic.min.css'
 import axios from "axios";
 import { show } from "./dialogs/Dialog";
+import { Container } from "semantic-ui-react";
 const BACKEND_BASE_URL = process.env.REACT_APP_BASE_URL;
 console.log(BACKEND_BASE_URL);
 
@@ -19,6 +20,17 @@ axios.interceptors.response.use(resp => {
     show(String(err), "发生错误", true);
 });
 const App: React.FC<{}> = () => {
-  return <Router></Router>;
+  return <div>
+    <Router></Router>
+    <Container textAlign="center">
+      <div style={{ color: "grey" }}>
+        By MikuNotFoundException<br></br>
+        <a href="https://github.com/Officeyutong/radiostation-web" target="_blank" rel="noopener noreferrer" >https://github.com/Officeyutong/radiostation-web</a><br></br>
+      Made with Love and React<br></br>
+      In memory of the past high school days<br></br>
+
+      </div>
+    </Container>
+  </div>;
 }
 export default App;
